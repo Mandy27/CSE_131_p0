c08671a5f8f8fa03c566c41511a999c413489545
Mandy Ngo <mango@ucsd.edu>
2016-01-12 12:03:05 -0800

    add type

--- test.sh
+++ test.sh
@@ -6,51 +6,51 @@ clear
 echo
 echo "             badbool.frag"
 ./glc < samples/badbool.frag >out.txt
-diff out.txt ../samples/badbool.out 
+diff out.txt samples/badbool.out 
 #cat out.txt
 
 echo
 echo "             float.frag"
 ./glc < samples/float.frag >out.txt
-diff out.txt ../samples/float.out 
+diff out.txt samples/float.out 
 #cat out.txt
 
 echo
 echo "             in.frag"
 ./glc < samples/in.frag >out.txt
-diff out.txt ../samples/in.out 
+diff out.txt samples/in.out 
 #cat out.txt
 
 
 echo
 echo "             layout.frag"
 ./glc < samples/layout.frag >out.txt
-diff out.txt ../samples/layout.out 
+diff out.txt samples/layout.out 
 #cat out.txt
 
 
 echo
 echo "             out.frag"
 ./glc < samples/out.frag >out.txt
-diff out.txt ../samples/out.out 
+diff out.txt samples/out.out 
 #cat out.txt
 
 
 echo
 echo "             switch.frag"
 ./glc < samples/switch.frag >out.txt
-diff out.txt ../samples/switch.out 
+diff out.txt samples/switch.out 
 #cat out.txt
 
 
 echo "             true.frag"
 ./glc < samples/true.frag >out.txt
-diff out.txt ../samples/true.out 
+diff out.txt samples/true.out 
 #cat out.txt
 
 
 echo "             while.frag"
 ./glc < samples/while.frag >out.txt
-diff out.txt ../samples/while.out 
+diff out.txt samples/while.out 
 #cat out.txt
 
