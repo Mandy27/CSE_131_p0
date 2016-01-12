d8cbd8436fd2027066a36b14edad68a8935fc61b
Tuan Nguyen <tcn006@ucsd.edu>
2016-01-12 12:41:29 -0800

    update test

--- test.sh
+++ test.sh
@@ -21,13 +21,24 @@ echo "             in.frag"
 diff out.txt samples/in.out 
 #cat out.txt
 
-
 echo
 echo "             layout.frag"
 ./glc < samples/layout.frag >out.txt
 diff out.txt samples/layout.out 
 #cat out.txt
 
+echo
+echo "             longid.frag"
+./glc < samples/longid.frag >out.txt
+diff out.txt samples/longid.out 
+#cat out.txt
+
+
+echo
+echo "             longid2.frag"
+./glc < samples/longid2.frag >out.txt
+diff out.txt samples/longid2.out 
+#cat out.txt
 
 echo
 echo "             out.frag"
