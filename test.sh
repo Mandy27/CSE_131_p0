70e29fcfc060f5a0ec0ac52f55f12ceed68f3bb7
Mandy Ngo <mango@ucsd.edu>
2016-01-13 22:18:34 -0800

    singleoperator

--- test.sh
+++ test.sh
@@ -10,52 +10,43 @@ echo "             badbool.frag"
 diff out.txt samples/badbool.out 
 #cat out.txt
 
-echo
 echo "             float.frag"
 ./glc < samples/float.frag >out.txt
 diff out.txt samples/float.out 
 #cat out.txt
 
-echo
 echo "             in.frag"
 ./glc < samples/in.frag >out.txt
 diff out.txt samples/in.out 
 #cat out.txt
 
-echo
 echo "             layout.frag"
 ./glc < samples/layout.frag >out.txt
 diff out.txt samples/layout.out 
 #cat out.txt
 
-echo
 echo "             longid.frag"
 ./glc < samples/longid.frag >out.txt
 diff out.txt samples/longid.out 
 #cat out.txt
 
 
-echo
 echo "             longid2.frag"
 ./glc < samples/longid2.frag &>out.txt
 diff out.txt samples/longid2.out 
 #cat out.txt
 
-echo
 echo "             out.frag"
 ./glc < samples/out.frag >out.txt
 diff out.txt samples/out.out 
 #cat out.txt
 
 
-echo
 echo "             switch.frag"
 ./glc < samples/switch.frag >out.txt
 diff out.txt samples/switch.out 
 #cat out.txt
 
-
-echo
 echo "             test.glsl"
 ./glc < samples/test.glsl >out.txt
 diff out.txt samples/test.out 
@@ -76,4 +67,16 @@ diff out.txt samples/while.out
 echo "             singlecomment.frag"
 ./glc < samples/singlecomment.frag >out.txt
 diff out.txt samples/singlecomment.out 
+#cat out.txt
+
+
+echo "             number.frag"
+./glc < samples/number.frag >out.txt
+diff out.txt samples/number.out 
+#cat out.txt
+
+
+echo "             singleoperator.frag"
+./glc < samples/singleoperator.frag >out.txt
+diff out.txt samples/singleoperator.out 
 #cat out.txt
